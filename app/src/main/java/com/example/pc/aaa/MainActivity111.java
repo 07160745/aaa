688e371fcfb67d7021a6252fecdc6a9dc9e94e4e
07160745 <2899029533@qq.com>
2017-10-19 09:42:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/pc/aaa/MainActivity111.java
@@ -0,0 +1,13 @@
+package com.example.pc.aaa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity111 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main111);
+    }
+}
